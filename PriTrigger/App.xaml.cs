596b1daa0343b1ee17327b9391b0162f5b0c1353
Tsuyoshi Kitano <tsuyoshi.kitano@gmail.com>
2021-02-15 21:09:07 +0900

    プロジェクト ファイルを追加します。

---
+++ PriTrigger/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Ioc;
+using PriTrigger.Views;
+using System.Windows;
+
+namespace PriTrigger
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
